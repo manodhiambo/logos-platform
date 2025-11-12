c722406561517128df7511f069753fe48693fdde
manodhiambo <manodhiambo@gmail.com>
2025-11-12 22:14:00 +0300

    adding more features

--- apps/backend/src/config/cloudinary.config.ts
+++ apps/backend/src/config/cloudinary.config.ts
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.U1pLQXicgTezq46ka-2mEcI5qds,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
